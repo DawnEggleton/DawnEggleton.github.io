8fc04c2073a8fdac0ea8816fb9c8af67f162e238
DawnEggleton <dawn.eggleton@carleton.ca>
2020-05-18 13:49:24 -0400

    Update script.js

--- jcink-temps/bea/script.js
+++ jcink-temps/bea/script.js
@@ -2,9 +2,11 @@ $('.bea-wrap.timeline h1').on('click', function() {
     if($(this).hasClass('yearOpen')) {
         $(this).next('.bea-content').slideUp();
         $(this).removeClass('yearOpen');
+        console.log('class removed');
     } else {
         $(this).next('.bea-content').slideDown();
         $(this).addClass('yearOpen');
+        console.log('class added');
     }
 });
 
