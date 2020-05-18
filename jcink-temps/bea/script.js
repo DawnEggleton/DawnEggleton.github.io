c93984caa4537c52eef4a7e35276bd28ab5a3de9
DawnEggleton <dawn.eggleton@carleton.ca>
2020-05-18 13:35:35 -0400

    bea tabbed template

--- jcink-temps/bea/script.js
+++ jcink-temps/bea/script.js
@@ -1,3 +1,11 @@
 $('.bea-wrap.timeline .bea-main h1').on('click', function() {
     $(this).next().slideToggle();
 });
+
+$('.tabbed .bea-notes span').on('click', function() {
+    console.log(this.id);
+    $('.tabbed .bea-notes span').removeClass('activeTab');
+    $('.tabbed .bea-main').removeClass('activeTab');
+    $(this).addClass('activeTab');
+    $('#' + this.id + '-content').addClass('activeTab');
+});
