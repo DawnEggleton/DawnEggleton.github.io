a54295a6b576a923c5ceb3d73e3b16321ea63f83
DawnEggleton <dawn.eggleton@carleton.ca>
2020-06-27 23:30:28 -0400

    Added Simple Template & Generator

---
+++ jcink-temps/simple/flexGen.js
@@ -0,0 +1,144 @@
+//general variables
+let type, colors, lHead = [''], sHead = [''], tempCode = '', tabs = 0;
+
+
+//Show/Hide Basic Fields
+showhide('lh', 'ifLH');
+showhide('sh', 'ifSH');
+
+//Show/Hide Complex Fields
+$('input[name="type"]').change(function () {
+	switch($(this).val()) {
+        case 'posting':
+            $('.typeSwitch').hide();
+            $('.ifPost').show();
+            break;
+        case 'tabbed': 
+            $('.typeSwitch').hide();
+            $('.ifTabbed').show();            
+            var html = '';
+            for (var i = 0; i < $('#tabCount').val(); i++) {
+                html += '<span class="twoCol altCol"><input id="tabTitle' + i + '" placeholder="Tab Title" /><textarea id="tabText' + i + '"placeholder="Tab Contents"></textarea></span>';
+            }
+            $('.tabContents').html(html);
+            break;
+        default:
+            console.log('template type: ' + $(this).val());
+            break;
+    }
+});
+
+$('input[name="tabCount"]').change(function() {
+    var html = '';
+    for (var i = 0; i < $(this).val(); i++) {
+        html += '<span class="twoCol altCol"><input id="tabTitle' + i + '" placeholder="Tab Title" /><textarea id="tabText' + i + '"placeholder="Tab Contents"></textarea></span>';
+    }
+    $('.tabContents').html(html);
+});
+
+
+//Set Variables
+function setValues() {
+
+    //set image
+    fromRadio('sh', sHead, '', '');
+    fromRadio('lh', lHead, '', '');
+    type = $('input[name="type"]:checked').val();
+    tabs = $('#tabCount').val();
+
+    //set up color styles
+    colors =        '<style>' +
+                    '.' + $('input[name="char"]').val() + ' .lux-temp1-container' +
+                    ' {--accent: ' +
+                    $('input[name="accent"]').val() +
+                    '}</style>';
+}
+
+
+//Build Final Content
+
+function setPostCode() {
+    console.log($('#postText').val());
+    var code =  '<span class="' +
+                $('input[name="char"]').val() +
+                '"><div class="lux-temp1-container lux-' + 
+                $('#tempSize :checked').val() +
+                '"><div class="lux-temp1-titlebox"><div class="lux-temp1-title"><span class="link">' + '\n\n' + 
+                sHead + '\n\n' + 
+                '</span>' + '\n\n' + 
+                lHead + '\n\n' +
+                '</div></div><div class="lux-temp1-contentbox"><div class="lux-temp1-content" id="scroll">\n' + 
+                $('#postText').val() + '\n\n' +
+                '</div></div></div></span><link href="//dawneggleton.github.io/jcink-temps/moments/styles.css" rel="stylesheet"><script src=//dawneggleton.github.io/jcink-temps/moments/scripts.js></script><link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">' +
+                colors;
+    return code;
+}
+
+function setTabbedCode() {
+    var code =  '<span class="' +
+                $('input[name="char"]').val() +
+                '"><div class="lux-temp1-container lux-' + 
+                $('#tempSize :checked').val() +
+                ' lux-tabbed"><div class="lux-temp1-titlebox">\n';
+
+
+    //do tab labels
+    for(var i = 0; i < $('#tabCount').val(); i++) {
+        if(i == 0) {
+            code += '<div class="lux-temp1-title lux-active"><span></span>' + $('#tabTitle' + i).val() + '</div>';
+        } else {
+            code += '<div class="lux-temp1-title"><span></span>' + $('#tabTitle' + i).val() + '</div>';
+        }
+    }
+
+    code += '</div><div class="lux-temp1-contentbox">';
+    
+    //code tabs
+    for(var i = 0; i < $('#tabCount').val(); i++) {
+        if(i == 0) {
+            code += '<div class="lux-temp1-content lux-active" id="scroll">' + $('#tabText' + i).val() + '</div>';
+        } else {
+            code += '<div class="lux-temp1-content" id="scroll">' + $('#tabText' + i).val() + '</div>';
+        }
+    }
+    
+    code += '<div class="lux-temp1-left"><i class="fas fa-caret-left"></i></div><div class="lux-temp1-right"><i class="fas fa-caret-right"></i></div></div></div></span><link href="//dawneggleton.github.io/jcink-temps/moments/styles.css" rel="stylesheet"><script src=//dawneggleton.github.io/jcink-temps/moments/scripts.js></script><link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">' + colors;
+    return code;
+}
+
+
+$('#runScript').on('click', function() {
+    let actTab = 1;
+    let titles = $('.lux-tabbed .lux-temp1-title');
+    let ttabs = $('.lux-tabbed .lux-temp1-content');
+
+    $('.lux-temp1-left').on('click', function(){	
+        if (actTab == 1) {
+            actTab = ttabs.length;
+        }
+        else {
+            actTab -= 1;
+        }
+        for (i = 0; i < ttabs.length; i++) {
+            $(titles[i]).removeClass('lux-active');
+            $(ttabs[i]).removeClass('lux-active');
+        }
+        $(titles[actTab - 1]).addClass('lux-active');
+        $(ttabs[actTab - 1]).addClass('lux-active');
+    });
+
+    $('.lux-temp1-right').on('click', function(){	
+        if (actTab == ttabs.length) {
+            actTab = 1;
+        }
+        else {
+            actTab += 1;
+        }
+        for (i = 0; i < ttabs.length; i++) {
+            $(titles[i]).removeClass('lux-active');
+            $(ttabs[i]).removeClass('lux-active');
+        }
+        $(titles[actTab - 1]).addClass('lux-active');
+        $(ttabs[actTab - 1]).addClass('lux-active');
+    });
+});
