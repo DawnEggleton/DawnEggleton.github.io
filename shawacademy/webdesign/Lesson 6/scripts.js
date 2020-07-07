4b22e3fd0d3a0f56705b8f086e465c33d3a51e0c
DawnEggleton <dawn.eggleton@carleton.ca>
2020-07-07 17:37:07 -0400

    update sslp character sheets

---
+++ shawacademy/webdesign/Lesson 6/scripts.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("hamburger").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("hamburger").style.display = "none";
+}
