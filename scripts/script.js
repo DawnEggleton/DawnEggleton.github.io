322ea165fd5919761e08748366accc4abd576dcc
DawnEggleton <dawn.eggleton@carleton.ca>
2021-02-18 22:37:54 -0500

    menu updates and project page template complete

--- scripts/script.js
+++ scripts/script.js
@@ -18,4 +18,37 @@ $(document).ready(function() {
             }
         });
     });
+
+
+
+    // Add smooth scrolling
+    $("a").on('click', function(event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('main').animate({
+                scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+            }, 1200, function(){
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+    //Image Lightbox
+    $('.gallery img').on('click', function() {
+        $('.overlay img').attr('src', $(this).attr('src'));
+        $('.overlay').fadeIn();
+    });
+    $('.overlay, .close').on('click', function() {
+        $('.overlay').fadeOut();
+    });
 });
