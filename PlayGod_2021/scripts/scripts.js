6534f8536accc8bf26949aaf15ea67e3f33330c9
DawnEggleton <dawn.eggleton@carleton.ca>
2021-05-24 21:11:37 -0400

    Upload Play God Skin Work

---
+++ PlayGod_2021/scripts/scripts.js
@@ -0,0 +1,50 @@
+function toggleInvert () {
+    if(document.querySelector('body').classList.contains('dark')) {
+        document.querySelector('body').classList.add('light');
+        document.querySelector('body').classList.remove('dark');
+        document.querySelector('#invertToggle').classList.remove('switched');
+    } else {
+        document.querySelector('body').classList.add('dark');
+        document.querySelector('body').classList.remove('light');
+        document.querySelector('#invertToggle').classList.add('switched');
+    }
+}
+function toggleFont () {
+    if(document.querySelector('body').classList.contains('lrgFont')) {
+        document.querySelector('body').classList.remove('lrgFont');
+        document.querySelector('#fontToggle').classList.remove('switched');
+    } else {
+        document.querySelector('body').classList.add('lrgFont');
+        document.querySelector('#fontToggle').classList.add('switched');
+    }
+}
+function togglePopNav() {
+    if(document.querySelector('.nav-popout').classList.contains('active')) {
+        document.querySelector('.nav-popout').classList.remove('active');
+    } else {
+        document.querySelector('.nav-popout').classList.add('active');
+    }
+}
+
+document.querySelectorAll('.info-labels a').forEach(label => {
+    label.addEventListener('click', (e) => {
+        document.querySelectorAll('.info-labels a').forEach(label => label.classList.remove('active'));
+        document.querySelectorAll('.info-tab').forEach(tab => tab.classList.remove('active'));
+        e.currentTarget.classList.add('active');
+        document.querySelector('#' + e.currentTarget.id + '-box').classList.add('active');
+    });
+});
+
+$(document).ready(function(){
+    $("a.anchorTag").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
