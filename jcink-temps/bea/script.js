834f2d063c51f572ca749d001bc46fdfe9436374
DawnEggleton <dawn.eggleton@carleton.ca>
2024-01-21 20:52:47 -0400

    fix bea templates

---
+++ jcink-temps/bea/script.js
@@ -0,0 +1,17 @@
+let years = document.querySelectorAll(`.bea-wrap.timeline h1`);
+if(years.length > 0) {
+    years.forEach(year => {
+        year.addEventListener('click', e=> {
+            let content = e.currentTarget.nextElementSibling;
+            $(content).slideToggle(300);
+        });
+    });
+}
+
+$('.bea-wrap.tabbed .bea-notes span').on('click', function() {
+    let id = this.id;
+    $(this).siblings().removeClass('activeTab');
+    $(this).parent().parent().siblings('.bea-main').removeClass('activeTab');
+    $(this).addClass('activeTab');
+    $(this).parent().parent().siblings('#' + id + '-content').addClass('activeTab');
+});
